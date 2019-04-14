c80f9e435f5b94834c118b2d9ab36af1782c656b
Jimmy-Bai <14843439+Jimmy-Bai@users.noreply.github.com>
2019-04-14 02:50:53 -0400

    Revert "Merge branch 'master' into Jim"
    This reverts commit bda5cc6e32e3db0dcda6a54d92efaf41e76be018, reversing
    changes made to 93c3d7bfa573a0c4cdd8b2dc668b83de623a44b0.

--- Finance-Genie/src/app/app-routing/app-routing.module.ts
+++ Finance-Genie/src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { AdminDashboard2Component } from './../admin/admin-dashboard2/admin-dashboard2.component';
 import { AdminDashboard1Component } from './../admin/admin-dashboard1/admin-dashboard1.component';
 import { StarterComponent } from './../starter/starter.component';
-import { MapsComponent } from './../maps/maps.component'
-import { AccountsComponent } from './../accounts/accounts.component'
 import { AdminComponent } from './../admin/admin.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,11 +9,8 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', redirectTo: 'maps', pathMatch: 'full' },
-      { path: 'overview', redirectTo: 'maps', pathMatch: 'full' },
-      { path: 'accounts', redirectTo: 'accounts', pathMatch: 'full' },
-      { path: 'maps', component: MapsComponent },
-      { path: 'accounts', component: AccountsComponent },
+      { path: '', redirectTo: 'starter', pathMatch: 'full' },
+      { path: 'starter', component: StarterComponent },
     ])
   ],
   declarations: [],
